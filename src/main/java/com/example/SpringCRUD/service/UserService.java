42e595fea4df615d40ca67989f47469783ff45ab
Kate Khovanskaya <KhovanskayaK@yandex.ru>
2024-02-21 14:06:55 +0300

    added logging aspect

--- src/main/java/com/example/SpringCRUD/service/UserService.java
+++ src/main/java/com/example/SpringCRUD/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.SpringCRUD.service;
 
+import com.example.SpringCRUD.aspects.TrackUserAction;
 import com.example.SpringCRUD.model.User;
 import com.example.SpringCRUD.repositories.UserRepository;
 import org.springframework.stereotype.Service;
@@ -13,20 +14,24 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-        public List<User> findAll(){
+    @TrackUserAction
+    public List<User> findAll(){
         return userRepository.findAll();
     }
+    @TrackUserAction
     public User saveUser(User user){
         return userRepository.save(user);
     }
+
     public void deleteById(int id){
         userRepository.deleteById(id);
     }
-
+    @TrackUserAction
     public User getOne(int id){
         System.out.println("User service get id: "+id);
         return userRepository.getOne(id);
     }
+
     public User updateUser(User user){
         return userRepository.update(user);
     }
