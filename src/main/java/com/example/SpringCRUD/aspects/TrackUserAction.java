27dad99d910876913aee81c4e5daef2d2db9caf8
Kate Khovanskaya <KhovanskayaK@yandex.ru>
2024-02-21 14:19:17 +0300

    added comments for aspects

--- src/main/java/com/example/SpringCRUD/aspects/TrackUserAction.java
+++ src/main/java/com/example/SpringCRUD/aspects/TrackUserAction.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для методов, с которыми будет работать аспект логирования
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface TrackUserAction {
